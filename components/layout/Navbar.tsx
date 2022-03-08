67df9ef781d533eacd73767814a6118e6e226f18
ckm960411 <a01098956117@gmail.com>
2022-03-08 14:00:04 +0900

    Refactor: Navbar 코드 리팩토링
    Navbar 코드 리팩토링
    - NavbarBtns 컴포넌트를 만들어 코드를 가독성있게 분리함

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,16 +1,11 @@
 import React, { FC, useState } from "react";
-import { useAppDispatch, useAppSelector } from "store/hooks";
-import { Badge, Button, IconButton, Stack, Toolbar, Tooltip, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { IconButton, Stack, Toolbar, Typography, useMediaQuery, useTheme } from "@mui/material";
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
 import MenuIcon from "@mui/icons-material/Menu";
-import MailIcon from "@mui/icons-material/Mail";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { styled } from "@mui/material/styles";
 import { drawerWidth } from "components/layout/AppLayout";
-import CertificationModal from "components/login/CertificationModal";
-import { authService } from "fireBaseApp/fBase";
 import { useRouter } from "next/router";
-import { removeMyInfoData } from "store/usersSlice";
+import NavbarBtns from "components/layout/NavbarBtns";
 
 interface AppBarProps extends MuiAppBarProps {
   open?: boolean;
@@ -38,48 +33,25 @@ type NavbarProps = {
   handleDrawerOpen: () => void;
 };
 
-export type CertificationType = "로그인" | "회원가입"
-
 const Navbar: FC<NavbarProps> = ({ open, handleDrawerOpen }) => {
   const router = useRouter()
-  const dispatch = useAppDispatch()
-  const [modalOpened, setModalOpened] = useState<boolean>(false)
-  const [certificationType, setCertificationType] = useState<CertificationType>("로그인")
   const theme = useTheme()
   const downMd = useMediaQuery(theme.breakpoints.down("md"))
-
-  const myInfo = useAppSelector(state => state.users.myInfo)
-
-  const handleModalOpen = (e: React.MouseEvent<HTMLElement>) => {
-    const { innerText } = e.target as HTMLElement
-    if (innerText === "로그인") setCertificationType('로그인')
-    else setCertificationType('회원가입')
-    setModalOpened(true)
-  }
-  const handleModalClose = () => setModalOpened(false)
-
-  const onLogoutClick = () => {
-    const ok = confirm('정말 로그아웃 하시겠습니까?')
-    if (!ok) return
-    authService.signOut()
-    alert('로그아웃이 완료됐습니다!')
-    dispatch(removeMyInfoData())
-  }
 
   return (
     <AppBar position="fixed" open={open} sx={{ backgroundColor: "#fff" }}>
       <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
         <Stack direction="row">
-          <>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              onClick={handleDrawerOpen}
-              edge="start"
-              sx={{ mr: 2, ...(open && { display: "none" }), color: "#009e5d" }}
-            >
-              <MenuIcon />
-            </IconButton>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: "none" }), color: "#009e5d" }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <div>
             {open || (
               <Typography
                 variant="h6"
@@ -90,52 +62,9 @@ const Navbar: FC<NavbarProps> = ({ open, handleDrawerOpen }) => {
                 모드락
               </Typography>
             )}
-          </>
+          </div>
         </Stack>
-        <div>{/** 로그인 박스가 우측에 가도록 삽입 */}</div>
-        {(downMd && open) || (
-          <Stack direction="row" spacing={2} sx={{ alignItems: "center" }}>
-            {myInfo ? (
-              <>
-                <Stack direction="row" spacing={1}>
-                  <Tooltip title="알림" arrow>
-                    <IconButton>
-                      <Badge color="primary" variant="dot" sx={{ cursor: "pointer" }}>
-                        <MailIcon sx={{ color: "#858585" }} />
-                      </Badge>
-                    </IconButton>
-                  </Tooltip>
-                  <Tooltip title="장바구니" arrow>
-                    <IconButton>
-                      <Badge color="primary" variant="dot" sx={{ cursor: "pointer" }}>
-                        <ShoppingCartIcon sx={{ color: "#858585" }} />
-                      </Badge>
-                    </IconButton>
-                  </Tooltip>
-                </Stack>
-                <Button variant="contained" size="small" onClick={onLogoutClick}>
-                  로그아웃
-                </Button>
-              </>
-            ) : (
-              <>
-                <Button variant="outlined" size="small" onClick={handleModalOpen}>
-                  회원가입
-                </Button>
-                <Button variant="contained" size="small" onClick={handleModalOpen}>
-                  로그인
-                </Button>
-                {modalOpened && (
-                  <CertificationModal 
-                    open={modalOpened} 
-                    handleClose={handleModalClose} 
-                    certificationType={certificationType} 
-                  />
-                )}
-              </>
-            )}
-          </Stack>
-        )}
+        <>{(downMd && open) || <NavbarBtns />}</>
       </Toolbar>
     </AppBar>
   );
---
+++ components/layout/NavbarBtns.tsx
@@ -0,0 +1,80 @@
+import { FC, useState } from "react";
+import { authService } from "fireBaseApp/fBase";
+import MailIcon from "@mui/icons-material/Mail";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Badge, Button, IconButton, Stack, Tooltip } from "@mui/material";
+import { useAppDispatch, useAppSelector } from "store/hooks";
+import { removeMyInfoData } from "store/usersSlice";
+import CertificationModal from "components/login/CertificationModal";
+
+export type CertificationType = "로그인" | "회원가입"
+
+const NavbarBtns: FC = () => {
+  const dispatch = useAppDispatch()
+  const [modalOpened, setModalOpened] = useState<boolean>(false)
+  const [certificationType, setCertificationType] = useState<CertificationType>("로그인")
+
+  const myInfo = useAppSelector(state => state.users.myInfo)
+
+  const handleModalOpen = (e: React.MouseEvent<HTMLElement>) => {
+    const { innerText } = e.target as HTMLElement
+    if (innerText === "로그인") setCertificationType('로그인')
+    else setCertificationType('회원가입')
+    setModalOpened(true)
+  }
+  const handleModalClose = () => setModalOpened(false)
+
+  const onLogoutClick = () => {
+    const ok = confirm('정말 로그아웃 하시겠습니까?')
+    if (!ok) return
+    authService.signOut()
+    alert('로그아웃이 완료됐습니다!')
+    dispatch(removeMyInfoData())
+  }
+
+  return (
+    <Stack direction="row" spacing={2} sx={{ alignItems: "center" }}>
+      {myInfo ? (
+        <>
+          <Stack direction="row" spacing={1}>
+            <Tooltip title="알림" arrow>
+              <IconButton>
+                <Badge color="primary" variant="dot" sx={{ cursor: "pointer" }}>
+                  <MailIcon sx={{ color: "#858585" }} />
+                </Badge>
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="장바구니" arrow>
+              <IconButton>
+                <Badge color="primary" variant="dot" sx={{ cursor: "pointer" }}>
+                  <ShoppingCartIcon sx={{ color: "#858585" }} />
+                </Badge>
+              </IconButton>
+            </Tooltip>
+          </Stack>
+          <Button variant="contained" size="small" onClick={onLogoutClick}>
+            로그아웃
+          </Button>
+        </>
+      ) : (
+        <>
+          <Button variant="outlined" size="small" onClick={handleModalOpen}>
+            회원가입
+          </Button>
+          <Button variant="contained" size="small" onClick={handleModalOpen}>
+            로그인
+          </Button>
+          {modalOpened && (
+            <CertificationModal
+              open={modalOpened}
+              handleClose={handleModalClose}
+              certificationType={certificationType}
+            />
+          )}
+        </>
+      )}
+    </Stack>
+  );
+};
+
+export default NavbarBtns;
