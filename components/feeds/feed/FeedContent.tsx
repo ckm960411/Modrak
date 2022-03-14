ea31f755c34437b09f14ac37c52c034e8e87cd26
ckm960411 <a01098956117@gmail.com>
2022-03-15 06:44:30 +0900

    Feat: 피드의 태그를 클릭하면 해당 태그로 재검색
    피드의 태그를 클릭하면 해당 태그로 재검색
    
    Resolve: #55

--- components/feeds/feed/FeedContent.tsx
+++ components/feeds/feed/FeedContent.tsx
@@ -1,7 +1,17 @@
 import { FC } from "react";
 import { CardContent, Chip, Stack, Typography } from "@mui/material";
+import { useAppDispatch } from "store/hooks";
+import { setSearchTagFilter } from "store/filterSlice";
+import { setIsInitialLoad } from "store/feedsSlice";
 
 const FeedContent: FC<{ text: string; tags: string[] }> = ({ text, tags }) => {
+  const dispatch = useAppDispatch()
+  
+  const onClickTag = (tag: string) => () => {
+    dispatch(setSearchTagFilter({ tag }))
+    dispatch(setIsInitialLoad(true))
+  }
+
   return (
     <>
       <CardContent>
@@ -10,7 +20,7 @@ const FeedContent: FC<{ text: string; tags: string[] }> = ({ text, tags }) => {
       {tags[0] && (
         <CardContent sx={{ pt: 0, pb: 0 }}>
           <Stack direction="row" spacing={1}>
-            {tags.map((tag, i) => <Chip key={i} label={tag} onClick={() => console.log(tag)} /> )}
+            {tags.map((tag, i) => <Chip key={i} label={tag} onClick={onClickTag(tag)} /> )}
           </Stack>
         </CardContent>
       )}
