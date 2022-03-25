433c9738ec5c23e21472b60a591c2c5d880ba870
ckm960411 <a01098956117@gmail.com>
2022-03-26 00:17:58 +0900

    Design: 숙소 정보 탭 UI 구현
    Resolve: #93, #95

--- components/accommodations/roomDetail/body/tabs/RoomInfo.tsx
+++ components/accommodations/roomDetail/body/tabs/RoomInfo.tsx
@@ -1,11 +1,71 @@
 import { FC } from "react";
+import { Divider, Stack, Typography } from "@mui/material";
+import styled from "@emotion/styled";
+import { useAppSelector } from "store/hooks";
+import Map from "components/parts/Map";
+import RoomIcon from "components/parts/RoomIcon";
 
 const RoomInfo: FC = () => {
+  const roomData = useAppSelector(state => state.rooms.roomData)
+  if (!roomData) return <div>Loading...</div>
+  const { address, description, checkin, checkout, convenience, service, notice } = roomData
+
   return (
     <>
-      숙소 정보
+      <DescContainer>
+        <DescHeader>숙소 소개</DescHeader>
+        <Description>
+          {description.map((desc, i) => <Description key={i}>{desc}</Description>)}
+        </Description>
+      </DescContainer>
+      <Divider />
+      <DescContainer>
+        <DescHeader>체크인/체크아웃</DescHeader>
+        <Description>체크인 - {checkin} / 체크아웃 - {checkout}</Description>
+      </DescContainer>
+      <Divider />
+      <DescContainer>
+        <DescHeader>편의 시설</DescHeader>
+        <Description>
+          {convenience.map((value, i) => <RoomIcon key={i} icon={value} />)}
+        </Description>
+      </DescContainer>
+      <Divider />
+      <DescContainer>
+        <DescHeader>서비스</DescHeader>
+        <Description>
+          {service.map((value, i) => <RoomIcon key={i} icon={value} />)}
+        </Description>
+      </DescContainer>
+      <Divider />
+      <DescContainer>
+        <DescHeader>이용 안내</DescHeader>
+        <Description>
+          {notice.map((value, i) => <Description key={i}>{value}</Description>)}
+        </Description>
+      </DescContainer>
+      <Divider />
+      <DescContainer>
+        <DescHeader>숙소 위치</DescHeader>
+        <Description style={{ marginBottom: '16px' }}>{address}</Description>
+        <Map address={address} />
+      </DescContainer>
     </>
   )
 }
+
+const DescContainer = styled(Stack)`
+  margin-top: 16px;
+  margin-bottom: 16px;
+`
+const DescHeader = styled(Typography)`
+  font-weight: 600;
+  color: #333;
+  margin-bottom: 10px;
+`
+const Description = styled.div`
+  font-size: 14px;
+  color: #333;
+`
 
 export default RoomInfo
---
+++ components/parts/RoomIcon.tsx
@@ -0,0 +1,51 @@
+import { FC } from "react";
+import { Typography } from "@mui/material";
+import styled from "@emotion/styled";
+
+const icons: {
+  [key: string]: string
+} = {
+  '노래방': "https://dffoxz5he03rp.cloudfront.net/icons/ic_sing_28x28_gray_700.svg",
+  '레스토랑': "https://dffoxz5he03rp.cloudfront.net/icons/ic_restaurant_28x28_gray_700.svg",
+  '매점/편의점': "https://dffoxz5he03rp.cloudfront.net/icons/ic_store_28x28_gray_700.svg",
+  '수영장': "https://dffoxz5he03rp.cloudfront.net/icons/ic_pool_28x28_gray_700.svg",
+  '워터파크': "https://dffoxz5he03rp.cloudfront.net/icons/ic_pool_28x28_gray_700.svg",
+  '유아시설': "https://dffoxz5he03rp.cloudfront.net/icons/ic_teddybear_28x28_gray_700.svg",
+  '키즈플레이룸': "https://dffoxz5he03rp.cloudfront.net/icons/ic_teddybear_28x28_gray_700.svg",
+  '카페': "https://dffoxz5he03rp.cloudfront.net/icons/ic_cafe_28x28_gray_700.svg",
+  '피트니스': "https://dffoxz5he03rp.cloudfront.net/icons/ic_gym_28x28_gray_700.svg",
+  '금연': "https://dffoxz5he03rp.cloudfront.net/icons/ic_nosmoking_28x28_gray_700.svg",
+  '라운지': "https://dffoxz5he03rp.cloudfront.net/icons/ic_cocktail_28x28_gray_700.svg",
+  '장애인': "https://dffoxz5he03rp.cloudfront.net/icons/ic_wheelchair_28x28_gray_700.svg",
+  '짐보관': "https://dffoxz5he03rp.cloudfront.net/icons/ic_baggage_28x28_gray_700.svg",
+  'wifi': "https://dffoxz5he03rp.cloudfront.net/icons/ic_wifi_28x28_gray_700.svg",
+  '주차가능': "https://dffoxz5he03rp.cloudfront.net/icons/ic_parking_28x28_gray_700.svg",
+  '무료주차': "https://dffoxz5he03rp.cloudfront.net/icons/ic_parking_28x28_gray_700.svg",
+  '조식서비스': "https://dffoxz5he03rp.cloudfront.net/icons/ic_breakfast_28x28_gray700.svg",
+  '루프탑': "https://dffoxz5he03rp.cloudfront.net/icons/ic_rooftop_28x28_gray_700.svg",
+  '취사가능': "https://dffoxz5he03rp.cloudfront.net/icons/ic_kitchen_28x28_gray_700.svg",
+  '비즈니스센터': "https://dffoxz5he03rp.cloudfront.net/icons/ic_businessbag_28x28_gray_700.svg",
+  '세미나실': "https://dffoxz5he03rp.cloudfront.net/icons/ic_seminar_28x28_gray_700.svg",
+}
+
+const RoomIcon: FC<{icon: string}> = ({ icon }) => {
+  return (
+    <>
+      <IconContainer>
+        <img alt={icon} src={icons[`${icon}`]} style={{ width: '30px' }} />
+        <Typography sx={{ fontSize: '14px' }}>{icon}</Typography>
+      </IconContainer>
+    </>
+  )
+}
+
+const IconContainer = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 80px;
+  height: 80px;
+`
+
+export default RoomIcon
