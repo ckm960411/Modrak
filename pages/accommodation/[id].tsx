d575f5301a25b97599c2bdc0c9ecdbfc671048d5
ckm960411 <a01098956117@gmail.com>
2022-03-29 20:00:46 +0900

    Fix: 숙소 상세페이지 접속시 로그인 상태가 풀리는 버그 해결
    숙소 상세페이지 접속시 로그인 상태가 풀리는 버그 해결
    
    Resolve: #117

--- pages/accommodation/[id].tsx
+++ pages/accommodation/[id].tsx
@@ -1,11 +1,15 @@
 import { FC, useEffect } from "react";
+import { browserSessionPersistence, onAuthStateChanged, setPersistence } from "firebase/auth";
+import { authService } from "fireBaseApp/fBase";
 import { useAppDispatch } from "store/hooks";
 import { addRoomInfo } from "store/slices/roomsSlice";
+import wrapper from "store/configureStore";
+import { loadMyInfoData } from "store/slices/usersSlice";
+import searchFirestoreDoc from "utils/functions/searchFirestoreDoc";
 import { getAccommodationById } from "utils/SSRFunctions/getAccommodationById";
 import { getAllAccommodationsId } from "utils/SSRFunctions/getAllAccommodationsId";
 import RoomInfoHeader from "@roomDetail/header/RoomInfoHeader";
 import AccommodationTabs from "@roomDetail/body/AccommodationTabs";
-import wrapper from "store/configureStore";
 
 const AccommodationDetail: FC<{data: AccommodationWithId}> = ({ data }) => {
   const dispatch = useAppDispatch()
@@ -13,6 +17,19 @@ const AccommodationDetail: FC<{data: AccommodationWithId}> = ({ data }) => {
   useEffect(() => {
     dispatch(addRoomInfo(data))
   }, [data, dispatch])
+
+  const onLoadUserData = async (uid: string) => {
+    const { searchedData: userData } = await searchFirestoreDoc(`users/${uid}`)
+    dispatch(loadMyInfoData(userData))
+  }
+
+  useEffect(() => {
+    setPersistence(authService, browserSessionPersistence)
+    onAuthStateChanged(authService, user => {
+      if (user) return onLoadUserData(user.uid)
+      else return
+    })
+  }, [])
 
   return (
     <div style={{ minWidth: '300px', maxWidth: '920px', margin: '0 auto' }}>
